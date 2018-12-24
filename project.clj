1a39db57be18f4e2af62b0161da1455cd8c86b7b
Bost <thebost@gmail.com>
2018-12-24 14:09:17 +0100

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject utils "0.9.3"
+(defproject utils "0.10.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
   [
-   [org.clojure/clojure "1.9.0"]
+   [org.clojure/clojure "1.10.0"]
    ;; java bytecode, no clojurescript
    #_[clj-time-ext "0.16.0"]
    ]
   :plugins
   [
    ;; collection of nREPL middleware designed to enhance CIDER
-   [cider/cider-nrepl "0.17.0" :exclusions [org.clojure/tools.nrepl]]])
+   [cider/cider-nrepl "0.18.0" :exclusions [org.clojure/tools.nrepl]]])
