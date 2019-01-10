78fee6c0498383104fceb24a996db4b5206fc5b7
Bost <thebost@gmail.com>
2019-01-10 15:54:04 +0100

    Use lein-v plugin

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utils "0.10.0"
+(defproject utils :lein-v
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -7,9 +7,11 @@
   [
    [org.clojure/clojure "1.10.0"]
    ;; java bytecode, no clojurescript
-   #_[clj-time-ext "0.16.0"]
+   #_[clj-time-ext "0.0.0-31-0x6e56"]
    ]
   :plugins
   [
+   ;; Drive leiningen project version from git instead of the other way around
+   [com.roomkey/lein-v "7.0.0"]
    ;; collection of nREPL middleware designed to enhance CIDER
    [cider/cider-nrepl "0.18.0" :exclusions [org.clojure/tools.nrepl]]])
