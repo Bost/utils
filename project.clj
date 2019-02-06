2be834ab05b9e8d6bb344f5a4e55ec1f878eb9b5
Bost <thebost@gmail.com>
2019-02-06 15:03:27 +0100

    Upgrade clj-time-ext

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   [[org.clojure/clojure "1.10.0"]
    [org.clojure/core.rrb-vector "0.0.13"]
    ;; clj-time-ext as well as clj-time produces java bytecode, no clojurescript
-   [clj-time-ext "0.0.0-32-0x20c9"]
+   [clj-time-ext "0.0.0-34-0x7939"]
    ;; File system utilities
    [me.raynes/fs "1.4.6"]]
    ;; can't use the lein-v from ~/.lein/profiles.clj
